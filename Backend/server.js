f067b3545b2fd2aca3feacaab47147af98197828
Prerna-09 <prerna09sijwali@gmail.com>
2024-06-03 17:23:15 +0530

    added vercel

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,13 @@ const dotenv = require("dotenv")
 dotenv.config();
 
 const cors = require("cors");
-app.use(cors())
+app.use(cors(
+  {
+    origin: ["https:://deploy-mern-1whq.vercel.app"],
+    methods:["POST", "GET", "DELETE" ,"PATCH"],
+    credentials:true
+  }
+))
 
 const userRoute = require("./routes/userRoute.js");
 
